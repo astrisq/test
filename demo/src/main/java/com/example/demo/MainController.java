956c5938ace02e3704a4e5d58dd9f28b9c849215
astriq <astriq@DESKTOP-303V3Q5.T-mobile.com>
2021-05-07 13:34:47 +0900

    test

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jdk.internal.org.jline.utils.Log;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -11,7 +12,7 @@ public class MainController {
 
 	@RequestMapping(value="/main")
 	public String main() {
-		
+		Log.info("dkdk");
 		return "main";
 	}
 }
